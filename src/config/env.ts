68299928d55f037fed0efb2867f12ca71a9df846
Avinash Poojary <avinashpoojary651@gmail.com>
2025-11-18 10:38:01 +0530

    contact form with nodemailer

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,8 +4,11 @@ export const env = {
   PORT: process.env.PORT || "4000",
   JWT_SECRET: process.env.JWT_SECRET || "changeme",
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "7d",
-  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:5173",
+  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:8080",
   NODE_ENV: process.env.NODE_ENV || "development",
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "**REMOVED**",
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "**REMOVED**",
+  EMAIL_USER: process.env.EMAIL_USER || "purplemerit9@gmail.com",
+  EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || "bkpgbencidbbpdwg",
+  EMAIL_FROM: process.env.EMAIL_FROM || "purplemerit9@gmail.com",
 };
